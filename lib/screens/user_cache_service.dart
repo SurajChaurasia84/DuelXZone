4ca3a5d1c2a481d5dbb0eb11e792c1b043f1f4f6
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-24 13:24:29 +0530

    feat: improve user cache service logic

--- lib/screens/user_cache_service.dart
+++ lib/screens/user_cache_service.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
 class UserCacheService {
@@ -8,6 +9,9 @@ class UserCacheService {
   static const _gameKey = 'user_game';
   static const _gameIdKey = 'user_game_id';
   static const _gameLevelKey = 'user_game_level';
+  static const _lastOpenRewardAtKey = 'user_last_open_reward_at';
+  static const _lastAdRewardAtKey = 'user_last_ad_reward_at';
+  static const _lastCheckInAtKey = 'user_last_check_in_at';
 
   static Future<Map<String, String>> load() async {
     final prefs = await SharedPreferences.getInstance();
@@ -19,6 +23,9 @@ class UserCacheService {
       'game': prefs.getString(_gameKey) ?? '',
       'gameId': prefs.getString(_gameIdKey) ?? '',
       'gameLevel': prefs.getString(_gameLevelKey) ?? '',
+      'lastOpenRewardAt': prefs.getString(_lastOpenRewardAtKey) ?? '',
+      'lastAdRewardAt': prefs.getString(_lastAdRewardAtKey) ?? '',
+      'lastCheckInAt': prefs.getString(_lastCheckInAtKey) ?? '',
     };
   }
 
@@ -33,6 +40,42 @@ class UserCacheService {
     if (data.containsKey('gameLevel')) {
       await prefs.setString(_gameLevelKey, data['gameLevel'].toString());
     }
+
+    if (data.containsKey('lastOpenRewardAt')) {
+      final val = data['lastOpenRewardAt'];
+      if (val is Timestamp) {
+        await prefs.setString(_lastOpenRewardAtKey, val.toDate().toIso8601String());
+      } else if (val is String) {
+        await prefs.setString(_lastOpenRewardAtKey, val);
+      }
+    }
+    if (data.containsKey('lastAdRewardAt')) {
+      final val = data['lastAdRewardAt'];
+      if (val is Timestamp) {
+        await prefs.setString(_lastAdRewardAtKey, val.toDate().toIso8601String());
+      } else if (val is String) {
+        await prefs.setString(_lastAdRewardAtKey, val);
+      }
+    }
+    if (data.containsKey('lastCheckInAt')) {
+      final val = data['lastCheckInAt'];
+      if (val is Timestamp) {
+        await prefs.setString(_lastCheckInAtKey, val.toDate().toIso8601String());
+      } else if (val is String) {
+        await prefs.setString(_lastCheckInAtKey, val);
+      }
+    }
+  }
+
+  static Future<void> saveSingle(String key, String value) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (key == 'lastOpenRewardAt') {
+      await prefs.setString(_lastOpenRewardAtKey, value);
+    } else if (key == 'lastAdRewardAt') {
+      await prefs.setString(_lastAdRewardAtKey, value);
+    } else if (key == 'lastCheckInAt') {
+      await prefs.setString(_lastCheckInAtKey, value);
+    }
   }
 
   static Future<void> clear() async {
@@ -44,5 +87,8 @@ class UserCacheService {
     await prefs.remove(_gameKey);
     await prefs.remove(_gameIdKey);
     await prefs.remove(_gameLevelKey);
+    await prefs.remove(_lastOpenRewardAtKey);
+    await prefs.remove(_lastAdRewardAtKey);
+    await prefs.remove(_lastCheckInAtKey);
   }
 }
