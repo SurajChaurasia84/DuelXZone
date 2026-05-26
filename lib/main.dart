7c7b70a4e1a6cb71edadca9881865480315384a1
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-05-27 00:48:24 +0530

    refactor: update app initialization flow

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,13 @@ import 'package:google_sign_in/google_sign_in.dart';
 
 import 'firebase_options.dart';
 import 'screens/app.dart';
+import 'services/ad_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  
+  // Start initializing ads and preloading early in the background
+  AdService().init();
   
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
