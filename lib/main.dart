52c772b40b5d66ced2758b69c777e6a2f50416e3
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-06-22 14:48:59 +0530

    refactor(app): update app initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:google_sign_in/google_sign_in.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'firebase_options.dart';
 import 'screens/app.dart';
+import 'screens/onboarding_screen.dart';
 import 'services/ad_service.dart';
 
 Future<void> main() async {
@@ -17,5 +19,9 @@ Future<void> main() async {
   );
   
   await GoogleSignIn.instance.initialize();
+  
+  final prefs = await SharedPreferences.getInstance();
+  OnboardingScreen.skipped.value = prefs.getBool('onboarding_skipped') ?? false;
+  
   runApp(const App());
 }
