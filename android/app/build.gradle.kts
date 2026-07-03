6326e250a432021e98d45c71f81324096faf839f
Suraj Chaurasia <suraj84chaurasia@gmail.com>
2026-07-03 17:45:57 +0530

    chore(android): update Android configuration

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 }
 
 android {
-    namespace = "com.duelxzone.duelxzone"
+    namespace = "com.duelxzone.dynastyx"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -31,7 +31,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.duelxzone.duelxzone"
+        applicationId = "com.duelxzone.dynastyx"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
